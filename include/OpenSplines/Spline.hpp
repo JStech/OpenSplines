58faecfafcd6c2f1262450c306df3a746ce4ee76
John Stechschulte <jstechschulte@nextdroid.com>
2023-06-25 09:53:56 -0600

    filling in some of the base class

--- include/OpenSplines/Spline.hpp
+++ include/OpenSplines/Spline.hpp
@@ -1,11 +1,22 @@
 #include <Eigen/Dense>
 
 namespace Splines {
-template <typename space, typename T>
+template <typename Real, typename T, typename Combine>
 class Spline {
+    public:
+        Spline(std::vector<T> control_points, std::vector<Real> knots) :
+            control_points_(control_points),
+            knots_(knots) {}
+        virtual void interpolate(std::vector<T> data) = 0;
+        virtual void interpolate(std::vector<T> data, std::vector<Real> parameters) = 0;
+        virtual void fit(std::vector<T> data) = 0;
+        virtual void fit(std::vector<T> data, std::vector<Real> parameters) = 0;
+        virtual T operator()(Real parameter) const = 0;
+        virtual std::string to_string() = 0;
 
     private:
-
+        std::vector<Real> knots_;
+        std::vector<T> control_points_;
 
 }
 }
